bb2e0e8dab5ce6cceba15c7e4afcc6cebd886f49
P.A. Masse <pa.masse@split.io>
2024-10-02 21:47:29 +0000

    make interval optional

--- tap_grafana/config.py
+++ tap_grafana/config.py
@@ -6,6 +6,6 @@ from voluptuous import Schema, Required, Optional
 CONFIG_CONTRACT = Schema([{
     Required('table_name'): str,
     Required('query'): str,
-    Required('interval'): str,
+    Optional('interval'): str,
     Optional('max_lookback_days'): int,
 }])
--- tap_grafana/grafana.py
+++ tap_grafana/grafana.py
@@ -99,7 +99,7 @@ def get_grafana_fields(config, query, from_time, to_time):
         fields.append({'name': 'time', 'fieldType': 'integer', 'keyField': True})
         for field in response[0]['metric']:
             fields.append({'name': field, 'fieldType': 'string', 'keyField': True})
-        fields.append({'name': 'value', 'fieldType': 'integer', 'keyField': False})
+        fields.append({'name': 'value', 'fieldType': 'string', 'keyField': False})
 
     return fields
 
@@ -115,7 +115,8 @@ def get_grafana_records(config, query, interval, from_time, to_time):
     
     params = {}
     params['query'] = query
-    params['step'] = interval
+    if interval:
+        params['step'] = interval
     params['start'] = from_time
     if to_time:
         params['end'] = to_time
